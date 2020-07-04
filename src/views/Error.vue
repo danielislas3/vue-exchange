40ed1487eec23b3830ca97a87db97cdaf93dedf0
danielislas3 <danielislas3@live.com>
2020-07-04 00:06:21 -0500

    vista detalle

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -3,6 +3,7 @@
     <h1 class="text-gray-700 text-6xl uppercase">Error :(</h1>
     <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/">
       Volver a la página de Inicio
+      
     </router-link>
   </div>
 </template>
