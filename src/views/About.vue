9bf47d66be064ce0b88109872590259f1fe6e4dd
danielislas3 <danielislas3@live.com>
2020-07-04 00:02:57 -0500

    vista detalle

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,9 +7,8 @@
       <br />
     </p>
 
-    <router-link
-      to="/"
-      class="mt-5 text-xl text-blue-600 hover:underline"
-    >Volver a la pagina de Inicio</router-link>
+    <router-link to="/" class="mt-5 text-xl text-blue-600 hover:underline"
+      >Volver a la pagina de Inicio</router-link
+    >
   </div>
 </template>
